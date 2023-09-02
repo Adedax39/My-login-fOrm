864a35537e7e26ce7f1cb1b15dba82cf0e5d90f7
Lasath Rathnayake <127961214+Adedax39@users.noreply.github.com>
2023-09-02 17:36:55 +0530

    Add files via upload

---
+++ dbo.Table_login.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [RegNo]         INT           NOT NULL,
+    [firstName]     NVARCHAR (50) NULL,
+    [lastName]      NVARCHAR (50) NULL,
+    [dateOfBirth]   NVARCHAR (50) NULL,
+    [address]       NVARCHAR (50) NULL,
+    [email]         NVARCHAR (50) NULL,
+    [mobilePhone]   INT           NULL,
+    [homePhone]     INT           NULL,
+    [parentName]    NVARCHAR (50) NULL,
+    [nic]           NVARCHAR (50) NULL,
+    [contactNumber] INT           NULL,
+    [gender]        NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([RegNo] ASC)
+);
+
